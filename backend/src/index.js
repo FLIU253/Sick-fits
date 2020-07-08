7ac60d39a68f1ed4bf80faf54cc225aee37f5c48
Franklin Liu <FLIU253@uwo.ca>
2020-07-08 15:32:58 -0400

    fixed cors issue

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,12 +7,22 @@ const server = createServer();
 
 //TODO Use express middleware to handle cookies (JWT)
 //TODO use express middleware to populate current user
-
+server.express.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:7777');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 server.start(
   {
     cors: {
       credentials: true,
-      origin: process.env.FRONTEND_URL,
+      origin: [process.env.FRONTEND_URL],
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
     },
   },
   (deets) => {
