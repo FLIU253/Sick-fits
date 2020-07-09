f043c3f380fb68dee2233060c47f29a531c30d1d
Franklin Liu <FLIU253@uwo.ca>
2020-07-09 13:45:17 -0400

    implemented signin and signout

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,11 +3,22 @@ const cookieParser = require('cookie-parser');
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
 const db = require('./db');
-
+const jwt = require('jsonwebtoken');
 const server = createServer();
 
 server.express.use(cookieParser());
-//TODO use express middleware to populate current user
+
+//decode the jwt so we can get the user id on each request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    //put the userid onto the req for future requests to access
+    req.userId = userId;
+  }
+
+  next();
+});
 
 server.express.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', process.env.FRONTEND_URL);
